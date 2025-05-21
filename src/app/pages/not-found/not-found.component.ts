ae2f11f06cf64ee1bef523a2ba026cb1ad3a2561
srivardhan-mri <srivardhan@mririce.com>
2025-05-21 11:57:59 +0530

    updated 404 page

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,11 +1,23 @@
+// src/app/pages/not-found/not-found.component.ts
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router'; // To link back to home
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.scss'
+  styleUrls: ['./not-found.component.scss']
 })
 export class NotFoundComponent {
+  // You can inject ActivatedRoute here if you want to display the path that was not found
+  // import { ActivatedRoute } from '@angular/router';
+  // constructor(private route: ActivatedRoute) {}
+  // pathNotFound: string = '';
+  // ngOnInit() {
+  //   this.pathNotFound = this.route.snapshot.url.join('/');
+  // }
 
+  constructor() { }
 }
